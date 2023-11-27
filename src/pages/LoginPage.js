61f4362f1a33eb330da76c478750e23188a42ff3
SOM SUBHRA DATTA <somsubhra109@gmail.com>
2023-11-27 10:16:38 +0530

    Final Change befor go live

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -83,8 +83,8 @@ const LoginPage = () => {
 
             </form>
             <div className={LoginPageCSS.created_by_Container}>
-                <h3 ><span style={{ fontWeight: "lighter" }}>Created By</span> Som Subhra Datta(Son of Rajat Subhra Datta)</h3>
-                <h3><span style={{ fontWeight: "lighter" }}>Supervised By</span> Supriya Roy Kanungoe</h3>
+                <h3 ><span style={{ fontWeight: "lighter" }}>Developed By</span> Som Subhra Datta</h3>
+                <h3><span style={{ fontWeight: "lighter" }}>Under the guidence of</span> Supriya Roy Kanungoe</h3>
             </div>
             <SuccessSnackbar state={open} setState={setOpen} successMessage={successMessage} />
             <ErrorSnackbar state={openError} setState={setOpenError} errorMessage={errorMessage} />
